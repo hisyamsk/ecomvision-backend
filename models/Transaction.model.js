d0cecdb78b38e807ab5c9de127a06cd7615310ae
Hisyam <hisyamskurniawan18@gmail.com>
2023-01-23 22:18:12 +0700

    fix: no timestamps on Transaction model

--- models/Transaction.model.js
+++ models/Transaction.model.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose';
 
-const TransactionSchema = new mongoose.Schema({
-  userId: String,
-  cost: String,
-  products: {
-    type: [mongoose.Types.ObjectId],
-    of: Number,
+const TransactionSchema = new mongoose.Schema(
+  {
+    userId: String,
+    cost: String,
+    products: {
+      type: [mongoose.Types.ObjectId],
+      of: Number,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const TransactionModel = mongoose.model('Transaction', TransactionSchema);
 export default TransactionModel;
